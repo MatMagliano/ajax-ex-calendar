100d902af94c3a013c4962a83c25d7a08264dfff
Matteo Magliano <matteo.magliano01@gmail.com>
2020-02-03 18:39:38 +0100

    stamopa a schermo

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 $(document).ready(function() {
   // console.log('ciao');
-  var date = moment("2018-01");
-  var dayIn = date.daysInMonth();
-  console.log(dayIn);
-  console.log(date);
-  for (var i = 1; i <=dayIn; i++) {
-    var dayMonth = date.format("DD MMMM");
-    console.log(dayMonth);
+  var date = moment("2018-01-01");
+  console.log(date.format());
+  var daysIn = date.daysInMonth();
+  console.log(daysIn);
+  // var dayMonth = date.format("ddd DD MMMM");
+  // console.log(dayMonth);
+
+  for (var i = 1; i <= daysIn; i++) {
+    console.log(i);
+    // var dayMonth = date.format("ddd DD MMMM");
+    // console.log(dayMonth);
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    var context = {
+      "date": i + ' ' + date.format(' MMMM'),
+    };
+    var html = template(context);
+    $('.gennaio').append(html);
+
+
+
 
   }
-
 
 
 
