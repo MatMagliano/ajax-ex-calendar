2008d47f10590035df9d61aa7adffba7b150045d
Matteo Magliano <matteo.magliano01@gmail.com>
2020-02-03 15:29:05 +0100

    giorni

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-  console.log('ciao');
-})
+  // console.log('ciao');
+  var date = moment("2018-01");
+  var dayIn = date.daysInMonth();
+  console.log(dayIn);
+  console.log(date);
+  for (var i = 1; i <=dayIn; i++) {
+    var dayMonth = date.format("DD MMMM");
+    console.log(dayMonth);
+
+  }
+
+
+
+
+
+
+
+
+
+
+});
