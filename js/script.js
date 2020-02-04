04f22a0f5eb91c1294aa77361a5775058e4a223a
Matteo Magliano <matteo.magliano01@gmail.com>
2020-02-04 17:27:09 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,35 +1,83 @@
 $(document).ready(function() {
-  // console.log('ciao');
-  var date = moment("2018-01-01");
-  console.log(date.format());
-  var daysIn = date.daysInMonth();
-  console.log(daysIn);
-  // var dayMonth = date.format("ddd DD MMMM");
-  // console.log(dayMonth);
+  var year = 2018;
+  var thisMonth = 0;
+  var date = moment({
+    year: year,
+    month: thisMonth
+  });
+  printMonth(date);
+  printHoliday(date);
+  $('.next').click(function () {
+    var thisMonth = $('h2').attr('data-this-month');
+    var date = moment(thisMonth).add(1, 'months');
+    console.log(date);
 
+    printMonth(date);
+    printHoliday(date);
+  });
+  $('.prev').click(function () {
+    var thisMonth = $('h2').attr('data-this-month');
+    var date = moment(thisMonth).subtract(1, 'months');
+    console.log(date);
+
+    printMonth(date);
+    printHoliday(date);
+  });
+
+});// chiusura ducument
+
+// --------- FUNCTION -------------
+function printMonth(month) {
+  $('.month').html('');
+  $('h2').text(month.format('MMMM YYYY'));
+  $('h2').attr('data-this-month', month.format('YYYY-MM'));
+
+
+  var daysIn = month.daysInMonth();
   for (var i = 1; i <= daysIn; i++) {
-    console.log(i);
-    // var dayMonth = date.format("ddd DD MMMM");
-    // console.log(dayMonth);
+    var num = addZero(i);
     var source = $("#entry-template").html();
     var template = Handlebars.compile(source);
     var context = {
-      "date": i + ' ' + date.format(' MMMM'),
+      date: month.format('YYYY-MM') + '-' + addZero(i) ,
+      days: month.format(num + '-MMMM-YYYY'),
     };
     var html = template(context);
-    $('.gennaio').append(html);
-
-
-
-
+    $('.month').append(html);
   }
+}
+
+function printHoliday(month) {
+  $.ajax(
+    {
+      url: "https://flynn.boolean.careers/exercises/api/holidays",
+      method: "GET",
+      data: {
+        year: month.year(),
+        month: month.month(),
+      },
+      success: function (data) {
+        var holiday = data.response;
+        for (var i = 0; i < holiday.length; i++) {
+          var thisHoliday = holiday[i];
+          var thisHolidayData = thisHoliday.date;
+          $('li[data-value="'+ thisHolidayData  +'"]').addClass('green');
+          $('li[data-value="'+ thisHolidayData  +'"]').find('.name_holiday').append(' -' + thisHoliday.name);
+        }
+
+      },
+      error: function (richiesta, stato, errori) {
+        alert("E' avvenuto un errore. " + errore);
+      }
+  });
+}
 
 
 
 
-
-
-
-
-
-});
+function addZero(gg) {
+  if (gg < 10) {
+    return '0' + gg;
+  }
+  return gg;
+}
